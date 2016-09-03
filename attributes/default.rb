88f2c3b9497424388eac1fc08b694048b1882d24
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-09-03 15:57:30 +0400

    Draft

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+id = 'themis-finals-api-auth-py-lib'
+
+default[id]['basedir'] = '/var/themis/finals/lib/themis-finals-api-auth-py'
+default[id]['user'] = 'vagrant'
+default[id]['group'] = 'vagrant'
+default[id]['github_repository'] = 'aspyatkin/themis-finals-api-auth-py'
+default[id]['revision'] = 'master'
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'themis-finals-api-auth-py-lib'
+description 'Installs and configures themis.finals.api.auth package'
+version '1.0.0'
+
+recipe 'themis-finals-api-auth-py-lib', 'Installs and configures themis.finals.api.auth package'
+depends 'latest-git', '~> 1.1.11'
+depends 'git2', '~> 1.0.0'
