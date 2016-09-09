5e7180e333c2db89f01feae1edac95ad9db8c493
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-09-09 22:38:31 +0400

    Use ssh-private-keys cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
 name 'themis-finals-api-auth-py-lib'
 description 'Installs and configures themis.finals.api.auth package'
-version '1.0.0'
+version '1.1.0'
 
 recipe 'themis-finals-api-auth-py-lib', 'Installs and configures themis.finals.api.auth package'
 depends 'latest-git', '~> 1.1.11'
 depends 'git2', '~> 1.0.0'
+depends 'ssh-private-keys', '~> 1.0.0'
