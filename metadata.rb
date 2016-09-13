9f5857b496253ebd4fad7062e7987aea90537626
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-09-13 17:09:59 +0400

    Minor alterations

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 name 'themis-finals-api-auth-py-lib'
 description 'Installs and configures themis.finals.api.auth package'
-version '1.1.0'
+version '1.1.1'
 
-recipe 'themis-finals-api-auth-py-lib', 'Installs and configures themis.finals.api.auth package'
+recipe 'themis-finals-api-auth-py-lib',
+       'Installs and configures themis.finals.api.auth package'
 depends 'latest-git', '~> 1.1.11'
 depends 'git2', '~> 1.0.0'
 depends 'ssh-private-keys', '~> 1.0.0'
