bd1fdd7b21fe805bc029ff32f4279b4e28e10c9a
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2017-09-04 21:06:44 +0400

    Make use of instance cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,5 @@
 id = 'themis-finals-api-auth-py-lib'
 
 default[id]['basedir'] = '/var/themis/finals/lib/themis-finals-api-auth-py'
-default[id]['user'] = 'vagrant'
-default[id]['group'] = 'vagrant'
 default[id]['github_repository'] = 'themis-project/themis-finals-api-auth-py'
 default[id]['revision'] = 'master'
--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,11 @@
 name 'themis-finals-api-auth-py-lib'
 description 'Installs and configures themis.finals.api.auth package'
-version '1.1.2'
+version '1.2.0'
 
 recipe 'themis-finals-api-auth-py-lib',
        'Installs and configures themis.finals.api.auth package'
-depends 'git', '~> 6.0.0'
+depends 'git', '~> 8.0.0'
 depends 'git2', '~> 1.0.0'
 depends 'ssh-private-keys', '~> 2.0.0'
-depends 'ssh_known_hosts', '~> 5.1.0'
+depends 'ssh_known_hosts', '~> 4.0.0'
+depends 'instance', '~> 1.0.0'
